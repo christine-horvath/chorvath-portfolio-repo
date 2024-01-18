31788c9f08ef1ea1799d7972092db3f2be86f936
Christine Horvath <stinehorvath@gmail.com>
2024-01-18 14:03:28 -0500

    responsive hero

--- portfolio/src/Hero.js
+++ portfolio/src/Hero.js
@@ -4,14 +4,14 @@ function Hero() {
     return (
       <div className="flex h-96 relative">
           <img className="w-full h-fit opacity-50" src={Image} alt="image"/>
-          <div className="block md:px-20 absolute top-1/2">
-            <h1 className="font-bold body-font font-kalnia text-6xl text-berry py-5">
+          <div className="block py-5 px-5 50:px-20 absolute top-1/4 50:top-1/2 100:top-full">
+            <h1 className="py-5 40:w-8/12 font-bold body-font font-kalnia text-6xl 80:text-8xl text-berry drop-shadow-[1px_1.2px_0px_rgba(255,255,255,0.8)]">
               Hi! I'm Christine.
             </h1>
             <hr className="w-10 h-1 rounded border-0 bg-berry mb-3"></hr>
-            <p className="w-6/12">
-              I'm a web developer and multi-passionate creative. I'm passionate about 
-              making the internet more accessible for all. 
+            <p className="40:w-6/12 100:text-3xl">
+              I'm a web developer and multi-passionate creative who's determined to 
+              make the web more accessible for all.
             </p>
           </div>
           
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -8,6 +8,28 @@ module.exports = {
       'berry': '#8A0049',
       'light-pink': '#FFE0F1'
     },
+    screens: {
+      '40': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      '50': '800px',
+      // => @media (min-width: 800px) { ... }
+
+      '60': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      '70': '1120px',
+      // => @media (min-width: 1120px) { ... }
+
+      '80': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '100': '1600px',
+      // => @media (min-width: 1600px) { ... }
+
+      '120': '1920px',
+      // => @media (min-width: 1920px) { ... }
+    },
     extend: {
       fontFamily: {
         'kalnia': ['Kalnia', 'serif']
